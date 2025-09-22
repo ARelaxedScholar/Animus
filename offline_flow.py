3df5243798eb3e2ce02934a0cf9d16e306f328ae
ARelaxedScholar <angemmanuel.kouakou+professional@gmail.com>
2025-09-21 21:51:10 -0400

    feat(semantic chunking): Naive semantic chunking is implemented, no error processing at this point, since I will worry about it when the retry is implemented. Will likely cycle back to it.

--- offline_flow.py
+++ offline_flow.py
@@ -1,11 +1,12 @@
+from utility import extract_json_from_codeblock, call_llm, semantic_chunking
 from io import UnsupportedOperation
 import nltk
+from nltk.tokenize import sent_tokenize
 from pocketflow import AsyncParallelBatchNode, AsyncFlow, Node
 from eigen_client.data_types import Document
-# Setting up punkt (for sentence splitting)
-nltk.download("punkt")
 
 RAW_TEXT_EXTENSIONS = ("md", "txt")
+
 
 # --- Document Level Flow ---
 class SentenceSplitter(Node): 
@@ -20,15 +21,13 @@ class SentenceSplitter(Node):
                 return f.read()
 
         elif extension == "pdf":
-            import fitz
+            import pymupdf
             raw_text = ""
-            with fitz.open(filename) as pdf_doc:
-                for page in pdf_doc:
-                    raw_text = page.get_text()
-                return raw_text
+            with pymupdf.open(filename) as doc:  # open document
+                text = chr(12).join([page.get_text() for page in doc])
+                return text
         else:
             raise UnsupportedOperation("Passed an unsupported file extension")
-
 
 
     def exec(self, prep_res):
@@ -37,16 +36,57 @@ class SentenceSplitter(Node):
 
     def post(self, shared, prep_res, exec_res):
         shared["sentences"] = exec_res
+        # Pass the filename to the end
+        shared["sentences"].append(self.params["filename"])
         
-class ChunkMaker(Node):
-    pass
+class DocumentMaker(Node):
+    def prep(self, shared):
+        sentences = shared["sentences"]
+        # We know it's there since previous node
+        filename = sentences.pop() 
+        return (filename, sentences)
 
+    def exec(self, prep_res):
+        import uuid
+        filename, sentences = prep_res
+        file_directory = "stuff" #self.params["file_directory"]
+
+        metadata = {
+                    "file_directory": file_directory,
+                    "filename" : filename, 
+                    "sentence_length" : len(sentences),
+                    "chunk_count": 0, # this will be overwritten
+                }
+
+        # Proceed to the actual semantic chunking
+        chunks, chunk_count = semantic_chunking(sentences)
+        metadata["chunk_count"] = chunk_count
+        documents = [ Document(id=uuid.uuid4(), data=chunk, metadata={**metadata, "chunk_number": chunk_number}) 
+                     for (chunk, chunk_number) in chunks ]
+        return documents
+
+    def post(self, shared, prep_res, exec_res):
+        shared["documents"] = exec_res
+
+        
 class Embedder(Node):
-    pass
+    def prep(self, shared):
+        return shared["documents"]
 
 class VectorStoreWriter(Node):
     pass
 
 
+# Downloading the punkt stuff if needed
+nltk.download("punkt")
+nltk.download("punkt_tab")
+# Testing the logic
+x = {}
+node = SentenceSplitter()
+node2 = DocumentMaker() 
+node.params["filename"] = "vectors.pdf"
+node.run(x)
+node2.run(x)
 
+print(x["documents"])
 
---
+++ utility.py
@@ -0,0 +1,83 @@
+# --- Utility Functions ---
+def call_llm(prompt):
+    from ollama import chat
+    response = chat(
+            model="phi3:mini",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return response.message.content
+
+
+def extract_json_from_codeblock(text: str):
+    import re
+    import json
+
+    json_codeblock_pattern = re.compile(r"```json\s*(.*?)\s*```", re.DOTALL)
+    match = json_codeblock_pattern.search(text)
+
+    if match:
+        json_content_str = match.group(1)
+        # Extracts and loads without error-handling
+        # This is meant to be handled by PocketFlow retry logic
+        return json.loads(json_content_str)
+    else:
+        return None
+
+def semantic_chunking(sentences):
+    import json
+    chunk_number = 0
+    prompt = """You are a SEMANTIC BOUNDARY DETECTOR.
+                When given to strings, your ONLY response is either:
+                True: These two strings together form ONE coherent thought 
+                False: These two strings represent TWO distinct ideas.
+
+                You will be given a group of sentences (might be just one) along
+                with one new sentence.
+
+                You will respond True, if the new string belongs semantically with the rest.
+                And False otherwise.
+
+                Your output will follow a JSON format as follows:
+              
+                {
+                    "response": True/False,
+                    "explanation": The why you think these two strings do or don't belong together
+                }"""
+    if not sentences:
+        raise ValueError("Passed empty list to semantic_chunking")
+    
+    cur_chunk = sentences[0]
+    
+    if len(sentences) == 1:
+        return ([(cur_chunk,1)], 1)
+
+    chunks = []
+
+    for (i, sentence) in enumerate(sentences[1:]):
+        response = call_llm(prompt + "\n\n" + json.dumps({"curr_chunk" : cur_chunk, "new_sentence": sentence}))
+
+        # We leave this to the retry logic 
+        # (This might be heavy for the retry though)
+        # I'll potentially benchmark this later
+        evaluation = extract_json_from_codeblock(response)
+        # If the LLM is a prick, we'll just assume we should split 
+        should_add_to_chunk = evaluation.get("response", False) if evaluation else False
+            
+        if should_add_to_chunk:
+            cur_chunk += "\n" + sentence
+        else:
+            chunk_number += 1
+            chunks.append((cur_chunk, chunk_number))
+            
+            # start the next chunk
+            cur_chunk = sentence
+    
+    # Cleanup (Adding the last chunk)
+    if cur_chunk:
+        chunk_number += 1
+        chunks.append((cur_chunk, chunk_number))
+
+    # Return the chunks and the number of "ideas" covered
+    return (chunks, chunk_number)
+
+
